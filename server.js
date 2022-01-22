9638ae613f32d90f421376fa1f82e19da8ed01eb
Charles DeBrosse <cmdebrosse@outlook.com?>
2022-01-22 11:21:36 -0500

    GET routes added to server.js

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-import express from 'express';
-import path from 'path';
-import fs from 'fs';
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3001;
+const rootDirectory = path.join(__dirname, '/public')
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(mainDir, 'notes.html'));
+  res.sendFile(path.join(rootDirectory, 'notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/db/db.json'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(rootDirectory, 'index.html'));
 });
 
 app.listen(PORT, () => {
