176e6bb444af2e5667309f5e79498089164ac527
Charles DeBrosse <cmdebrosse@outlook.com?>
2022-01-22 11:09:35 -0500

    files reorganized

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ import fs from 'fs';
 const app = express();
 const PORT = 3001;
 
-app.get("/notes", (req, res) => {
+app.get('/notes', (req, res) => {
   res.sendFile(path.join(mainDir, 'notes.html'));
 });
 
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/db/db.json'));
+});
 
 app.listen(PORT, () => {
   console.log(`Now listening on port ${PORT}`);
